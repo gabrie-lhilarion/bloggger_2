31c920e7c4778e1401b46f4b25607ba3bf1ae644
gabrielHilarion <gabrielhhilarion@gmail.com>
2020-08-04 23:51:44 +0100

    created show, new, index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
     def index
         @articles = Article.all
     end
+
+   
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,16 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+    def show
+        @articles = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to article_path(@article)
+    end
 end
